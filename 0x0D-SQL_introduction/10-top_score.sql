073c090d56f1c456f26bc6a3ac64383260f2b3aa
sujh8 <suhajhm@yahoo.com>
2024-02-15 11:00:08 -0800

    sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,1 +1,2 @@
+-- List all_records_in 'second_table' of database 'hbtn_0c_0'
 SELECT score, name FROM second_table ORDER BY score DESC;
--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,1 +1,2 @@
+--of databse 'hbtn_0c_0' in my MySQL_server
 SELECT COUNT(*) FROM first_table WHERE id = 89;
